57b01526deb92abd642dd38d7baf8f156f5680cc
SebastianOrtiz <impactotenisok@gmail.com>
2023-04-11 12:59:45 -0300

    Mi primer commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto")
+
+/*Esta es la parte intermedia de nuestro proyecto
+En esta parte realizamos los chequeos necesarios
+Por último vamos a la última linea de saludo
+Todo esto está comentado
+
+y esto también
+
+*/
+
+
+// Este es el mensaje de despedida
+
+console.log("Adios")
