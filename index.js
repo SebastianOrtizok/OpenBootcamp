66557c7154805dcd2b06773fb6d0b25a1dcc4799
SebastianOrtiz <impactotenisok@gmail.com>
2023-04-11 15:50:13 -0300

    ver2

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 
 // Este es el mensaje de bienvenida
+
 console.log("Esta es la puerta de entrada al proyecto")
 
-/*Esta es la parte intermedia de nuestro proyecto
-En esta parte realizamos los chequeos necesarios
-Por último vamos a la última linea de saludo
-Todo esto está comentado
+/*
+    Esta es la parte intermedia de nuestro proyecto
+    En esta parte realizamos los chequeos necesarios
+    Por último vamos a la última línea de saludo
+    Esto también está comentado
+    Aquí también
+ */
 
-y esto también
-
-*/
-
-
-// Este es el mensaje de despedida
-
-console.log("Adios")
+// Este va a ser el mensaje que se envía para despedir
+console.log("Adiós")
